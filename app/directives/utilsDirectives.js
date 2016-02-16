693b96008da1531e8f606405de436cb2701bb912
radjosue <suejo.r15@gmail.com>
2016-02-16 12:59:34 -0600

    Se actualizan directivas

--- app/directives/utilsDirectives.js
+++ app/directives/utilsDirectives.js
@@ -1,139 +1,184 @@
-define([ 'modules/app', 'jquery', 'jquery-ui'], function(app) {
+/**
+ * @author      : <Josue Ramirez Davila>
+ * @mail        : <josueramirezdavila@gmail.com>
+ *
+ * @description : Directivas HTML de utilerias.
+ *
+ */
+
+define(['modules/app'], function(app) {
 
   /* INICIAN CONSTANTES QUE DEFINEN LA ESTRUCTURA COMUN DE LOS ALERTS */
   var HEADER_ALERTS         = '<div ng-show=¬¬SHOW_ATTRIBUTE¬¬ class="alert alert-¬¬ALERT_TYPE¬¬ alert-dismissable">';
-  var BODY_ALERTS           = '<div ng-transclude></div>';
   var BUTTON_DISMISS_ALERT  = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">'
                             + '&times;</span>'
                             + '</button>';
+  var BODY_ALERTS           = '<div ng-transclude></div>';
   var END_ALERT             = '</div>';
   /* FIN CONSTANTES COMUNES DE LOS ALERTS */
 
-  /* Directiva que genera un div de alert */
+  /**
+   * @author <Josue Ramirez Davila>
+   * @mail <josueramirezdavila@gmail.com>
+   *
+   * @description         : CONSTRUYE UN ALERT BOOTSTRAP
+   * @param showAttribute : Nombre del modelo con el cual se estara mostrando u ocultando el alert
+   * @param alertType     : Tipo de alerta que se quiere construir (danger, warning, info, success)
+   * @param isDismiss     : Bandera que define si el alert sera dismissable o no ('true', 'false')
+   *
+   */
+  function buildAlert(showAttribute, alertType, isDismiss) {
+    var template  = HEADER_ALERTS.replace("¬¬SHOW_ATTRIBUTE¬¬", showAttribute);
+    template      = template.replace("¬¬ALERT_TYPE¬¬", alertType);
+    if (isDismiss == 'true') {
+      template += BUTTON_DISMISS_ALERT;
+    }
+    template += BODY_ALERTS + END_ALERT;
+    return template;
+  }
+
+  /**
+   * @author <Josue Ramirez Davila>
+   * @mail <josueramirezdavila@gmail.com>
+   *
+   * @description       : CONSTRUYE UN ALERT BOOTSTRAP
+   * @attr show         : Nombre del modelo con el cual se estara mostrando u ocultando el alert
+   * @attr type         : Tipo de alerta que se quiere construir (danger, warning, info, success)
+   * @attr dismissable  : Bandera que define si el alert sera dismissable o no ('true', 'false')
+   *
+   */
   app.directive("alert", function(){
     return {
       restrict: "E",
       transclude: true,
       template: function(element, attr) {
-        var template  = HEADER_ALERTS.replace("¬¬SHOW_ATTRIBUTE¬¬", attr.show);
-        template      = template.replace("¬¬ALERT_TYPE¬¬", attr.type);
-        if (attr.dismissable == 'true') {
-          template += BUTTON_DISMISS_ALERT;
-        }
-        template += BODY_ALERTS + END_ALERT;
-        return template;
+        return buildAlert(attr.show, attr.type, attr.dismissable);
       }
     };
   });
 
-	/* Directiva que genera un div de alert de error */
+	/**
+   * @author <Josue Ramirez Davila>
+   * @mail <josueramirezdavila@gmail.com>
+   *
+   * @description       : CONSTRUYE UN ALERT BOOTSTRAP TIPO DANGER
+   * @attr show         : Nombre del modelo con el cual se estara mostrando u ocultando el alert
+   * @attr dismissable  : Bandera que define si el alert sera dismissable o no ('true', 'false')
+   *
+   */
 	app.directive("alertDanger", function(){
 		return {
 			restrict: "E",
       transclude: true,
 			template: function(element, attr) {
-        var template  = HEADER_ALERTS.replace("¬¬SHOW_ATTRIBUTE¬¬", attr.show);
-        template      = template.replace("¬¬ALERT_TYPE¬¬", "danger");
-        if (attr.dismissable == 'true') {
-          template += BUTTON_DISMISS_ALERT;
-        }
-        template += BODY_ALERTS + END_ALERT;
-        return template;
+        return buildAlert(attr.show, "danger", attr.dismissable);
 			}
 		};
 	});
 
-  /* Directiva que genera un div de alert de warning */
+  /**
+   * @author <Josue Ramirez Davila>
+   * @mail <josueramirezdavila@gmail.com>
+   *
+   * @description       : CONSTRUYE UN ALERT BOOTSTRAP TIPO WARNING
+   * @attr show         : Nombre del modelo con el cual se estara mostrando u ocultando el alert
+   * @attr dismissable  : Bandera que define si el alert sera dismissable o no ('true', 'false')
+   *
+   */
   app.directive("alertWarning", function(){
     return {
       restrict: "E",
       transclude: true,
       template: function(element, attr) {
-        var template  = HEADER_ALERTS.replace("¬¬SHOW_ATTRIBUTE¬¬", attr.show);
-        template      = template.replace("¬¬ALERT_TYPE¬¬", "warning");
-        if (attr.dismissable == 'true') {
-          template += BUTTON_DISMISS_ALERT;
-        }
-        template += BODY_ALERTS + END_ALERT;
-        return template;
+        return buildAlert(attr.show, "warning", attr.dismissable);
       }
     };
   });
 
-  /* Directiva que genera un div de alert de info */
+  /**
+   * @author <Josue Ramirez Davila>
+   * @mail <josueramirezdavila@gmail.com>
+   *
+   * @description       : CONSTRUYE UN ALERT BOOTSTRAP TIPO INFO
+   * @attr show         : Nombre del modelo con el cual se estara mostrando u ocultando el alert
+   * @attr dismissable  : Bandera que define si el alert sera dismissable o no ('true', 'false')
+   *
+   */
   app.directive("alertInfo", function(){
     return {
       restrict: "E",
       transclude: true,
       template: function(element, attr) {
-        var template  = HEADER_ALERTS.replace("¬¬SHOW_ATTRIBUTE¬¬", attr.show);
-        template      = template.replace("¬¬ALERT_TYPE¬¬", "info");
-        if (attr.dismissable == 'true') {
-          template += BUTTON_DISMISS_ALERT;
-        }
-        template += BODY_ALERTS + END_ALERT;
-        return template;
+        return buildAlert(attr.show, "info", attr.dismissable);
       }
     };
   });
 
-	/* Directiva que genera un div de alert de exito */
+	/**
+   * @author <Josue Ramirez Davila>
+   * @mail <josueramirezdavila@gmail.com>
+   *
+   * @description       : CONSTRUYE UN ALERT BOOTSTRAP TIPO SUCCESS
+   * @attr show         : Nombre del modelo con el cual se estara mostrando u ocultando el alert
+   * @attr dismissable  : Bandera que define si el alert sera dismissable o no ('true', 'false')
+   *
+   */
 	app.directive("alertSuccess", function(){
 		return {
 			restrict: "E",
       transclude: true,
 			template: function(element, attr) {
-				var template  = HEADER_ALERTS.replace("¬¬SHOW_ATTRIBUTE¬¬", attr.show);
-        template      = template.replace("¬¬ALERT_TYPE¬¬", "success");
-        if (attr.dismissable == 'true') {
-          template += BUTTON_DISMISS_ALERT;
-        }
-        template += BODY_ALERTS + END_ALERT;
-        return template;
+				return buildAlert(attr.show, "success", attr.dismissable);
 			}
 		};
 	});
 
-	/* Directiva que genera una ventana tipo 'modal' */
+	/**
+   *
+   * @description   : CONSTRUYE UN MODAL BOOTSTRAP
+   * @attr title    : Titulo del modal
+   * @attr visible  : Bandera que define si el modal se muestra o no ('true', 'false')
+   *
+   */
 	app.directive('modal', function () {
-     return {
-       template: '<div class="modal fade">' + 
-             '<div class="modal-dialog">' + 
-               '<div class="modal-content">' + 
-                 '<div class="modal-header">' + 
-                   '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
-                   '<h4 class="modal-title">{{ title }}</h4>' + 
-                 '</div>' + 
-                 '<div class="modal-body" ng-transclude></div>' + 
-               '</div>' + 
-             '</div>' + 
-           '</div>',
-       restrict: 'E',
-       transclude: true,
-       replace:true,
-       scope:true,
-       link: function postLink(scope, element, attrs) {
-         scope.title = attrs.title;
+    return {
+      template: '<div class="modal fade">' + 
+            '<div class="modal-dialog">' + 
+              '<div class="modal-content">' + 
+                '<div class="modal-header">' + 
+                  '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                  '<h4 class="modal-title">{{ title }}</h4>' + 
+                '</div>' + 
+                '<div class="modal-body" ng-transclude></div>' + 
+              '</div>' + 
+            '</div>' + 
+          '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
 
-         scope.$watch(attrs.visible, function(value){
-           if(value == true)
-             $(element).modal('show');
-           else
-             $(element).modal('hide');
-         });
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
 
-         $(element).on('shown.bs.modal', function(){
-           scope.$apply(function(){
-             scope.$parent[attrs.visible] = true;
-           });
-         });
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
 
-         $(element).on('hidden.bs.modal', function(){
-           scope.$apply(function(){
-             scope.$parent[attrs.visible] = false;
-           });
-         });
-       }
-     };
-   });
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+  });
 });
